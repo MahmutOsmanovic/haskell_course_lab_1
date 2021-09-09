3191922a1e2b95ad595144fe1cde626bdeb3c697
Your Name <mahmut.osmanovic@outlook.com>
2021-09-09 12:25:14 +0200

    solved question 3 and updated all question answeres to be in a pattern matching format over guards

--- labOne.hs
+++ labOne.hs
@@ -1,41 +1,48 @@
 -- QUESTION 1:
-
-makeRec :: Floating a => a -> a -> (Char,a,a)
+makeRec :: (Floating a, Ord a) => a -> a -> (Char,a,a)
 makeRec a b = ('R',a,b) -- z = a + b*i
 
 makePol :: (Floating a, Ord a) => a -> a -> (Char,a,a)
 makePol r v 
     | v < 0 || v>2*pi = error "Please, insert an angle v in the range of [0,2pi]"
     | r < 0 = error "Please, insert a distance r ub the range of r>= 0"
-    | otherwise = ('P', r, v) -- z = re^(iv)
+    | otherwise = ('P',r,v) -- z = re^(iv)
 
-first :: (a, b, c) -> a
-first (x, _, _) = x
+first :: (a,b,c) -> a
+first (x,_,_) = x
 
-second :: (a, b, c) -> b
-second (_, y, _) = y
+second :: (a,b,c) -> b
+second (_,y,_) = y
 
-third :: (a, b, c) -> c
-third (_, _, z) = z
+third :: (a,b,c) -> c
+third (_,_,z) = z
 
 -- QUESTION 2:
 getRe :: (Char, Float, Float) -> Float
-getRe threeTuple
-    | first threeTuple == 'R' = second threeTuple
-    | first threeTuple == 'P' = second threeTuple * cos (third threeTuple)
-    | otherwise = error "Invalid format. Correct format:\n \t('R' or 'P', float, float), R = Rectangular, P = Polar, with positive angle"
+getRe ('R',a,_) = a
+getRe ('P',r,v) = r * cos v
+getRe _ = error "Invalid format. Correct format:\n \t('R' or 'P', float, float), R = Rectangular, P = Polar, with positive angle"
 
 getIm :: (Char, Float, Float) -> Float
-getIm threeTuple
-    | first threeTuple == 'R' = third threeTuple
-    | first threeTuple == 'P' = second threeTuple * sin (third threeTuple)
-    | otherwise = error "Invalid format. Correct format:\n \t('R' or 'P', float, float), R = Rectangular, P = Polar, with positive angle"
+getIm ('R',_,b) = b
+getIm ('P',r,v) = r * sin v
+getIm _ = error "Invalid format. Correct format:\n \t('R' or 'P', float, float), R = Rectangular, P = Polar, with positive angle"
 
 getDist :: (Char, Float, Float) -> Float
-getDist threeTuple 
-    | first threeTuple == 'R' = sqrt(second threeTuple ^2 + third threeTuple ^2)
-    | first threeTuple == 'P' = second threeTuple
-    | otherwise = error "Invalid format. Correct format:\n \t('R' or 'P', float, float), R = Rectangular, P = Polar, with positive angle"
+getDist ('R',a,b) = sqrt(second threeTuple ^2 + third threeTuple ^2)
+getDist ('P',r,_) = r
+getDist _ = error "Invalid format. Correct format:\n \t('R' or 'P', float, float), R = Rectangular, P = Polar, with positive angle"
+
+getAngle :: (Char, Float, Float) -> Float
+getAngle ('R',a,b) 
+    | a > 0 && b == 0 = 0
+    | a < 0 && b == 0 = pi
+    | b > 0 = atan2 b a
+    | b < 0 = 2*pi - abs(atan2 b a)
+getAngle ('P',r,v)
+    | v > 0 = 0
+getAngle _ = error "Invalid format. Correct format:\n \t('R' or 'P', float, float), R = Rectangular, P = Polar, with positive angle"
+
 
 getAngle :: (Char, Float, Float) -> Float
 getAngle threeTuple
@@ -47,3 +54,26 @@ getAngle threeTuple
     | otherwise = error "Invalid format. Correct format:\n \t('R' or 'P', float, float), R = Rectangular, P = Polar, with positive angle"
 
 -- QUESTION 3:
+toRec :: (Char, Float, Float) -> (Char, Float, Float)
+toRec ('R',a,b) = ('R',a,b)
+toRec ('P',r,v) = makeRec r v
+toRec _ =  error "Invalid format. Correct format:\n \t('R' or 'P', float, float), R = Rectangular, P = Polar, with positive angle"
+
+toPol :: (Char, Float, Float) -> (Char, Float, Float)
+toPol ('R',a,b) = makePol a b
+toPol ('P',r,v) = ('P',r,v)
+toPol _ = error "Invalid format. Correct format:\n \t('R' or 'P', float, float), R = Rectangular, P = Polar, with positive angle"
+
+-- QUESTION 4
+compAdd :: (Char, Float, Float) -> String
+-- compAdd threeTuple 
+
+
+compSub :: (Char, Float, Float) -> String
+compSub 
+
+compMult :: (Char, Float, Float) -> String
+compMult
+
+compDiv :: (Char, Float, Float) -> String
+compDiv
