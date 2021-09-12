666f4afd91226c57c428fe228bffb2f583e88fce
Linkan42 <sali20jt@student.ju.se>
2021-09-12 18:44:36 +0200

    task 4 completed and tested

--- lab1_Linus.hs
+++ lab1_Linus.hs
@@ -61,44 +61,68 @@ toPol ('P',r,v) = ('P',r,v)             --tested
 toPol _ = error "Invalid argument"      --tested
 
 --Task 4
---compAdd :: Cplex -> Cplex -> Cplex
---compAdd ('R',a,b) ('R',c,d) = ('R',a + c,b + d)
---compAdd ('R',a,b) pol@('P',r,v) = ('R',a + (getRe pol),b + (getIm pol))
---compAdd pol@('P',r,v) ('R',a,b) = ('R',(getRe pol) + a,(getIm pol) + b)
---compAdd pol1@('P',r1,v1) pol2@('P',r2,v2) = ('R',(getRe pol1) + (getRe pol2),(getIm pol1) + (getIm pol2))
---compAdd _ _ = error "Invalid argument/s"
+compAdd :: Cplex -> Cplex -> Cplex
+compAdd ('R',a,b) ('R',c,d) = ('R',a + c,b + d)                                                             --tested
+compAdd ('R',a,b) pol@('P',r,v) = ('R',a + getRe pol,b + getIm pol)                                         --tested
+compAdd pol@('P',r,v) ('R',a,b) = ('R',getRe pol + a,getIm pol + b)                                         --tested
+compAdd pol1@('P',r1,v1) pol2@('P',r2,v2) = ('R',getRe pol1 + getRe pol2,getIm pol1 + getIm pol2)           --tested
+compAdd _ _ = error "Invalid argument/s"                                                                    --tested
 
---compSub :: Cplex -> Cplex -> Cplex  --TODO fix
---compSub ('R',a,b) ('R',c,d) = ('R',a - c,b - d)
---compSub ('R',a,b) pol@('P',r,v) = ('R',a - (getRe pol),b - (getIm pol))
---compSub pol@('P',r,v) ('R',a,b) = ('R',(getRe pol) - a,(getIm pol) - b)
---compSub pol1@('P',r1,v1) pol2@('P',r2,v2) = ('R',(getRe pol1) - (getRe pol2),(getIm pol1) - (getIm pol2))
---compSub _ _ = error "Invalid argument/s"
+compSub :: Cplex -> Cplex -> Cplex
+compSub ('R',a,b) ('R',c,d) = ('R',a - c,b - d)                                                             --tested
+compSub ('R',a,b) pol@('P',r,v) = ('R',a - getRe pol,b - getIm pol)                                         --tested
+compSub pol@('P',r,v) ('R',a,b) = ('R',getRe pol - a,getIm pol - b)                                         --tested
+compSub pol1@('P',r1,v1) pol2@('P',r2,v2) = ('R',getRe pol1 - getRe pol2,getIm pol1 - getIm pol2)           --tested
+compSub _ _ = error "Invalid argument/s"                                                                    --tested                                              
 
---compMult :: Cplex -> Cplex -> Cplex
---compMult rect1@('R',a,b) rect2@('R',c,d) = 
---    let pol1 = toPol rect1
---        pol2 = toPol rect2
---    in ('P',(getDist pol1) * (getDist pol2),(getAngle pol1) + (getAngle pol2))
---compMult rect@('R',a,b) ('P',r,v) = 
---    let pol = toPol rect
---    in ('P',r * (getDist pol),v + (getAngle pol))
---compMult ('P',r,v) rect@('R',a,b) = 
---    let pol = toPol rect
---    in ('P',r * (getDist rect),v + (getAngle pol))
---compMult ('P',r1,v1) ('P',r2,v2) = ('P',r1 * r2,v1 + v2)
---compMult _ _ = error "Invalid argument/s"
+angleRem :: Double -> Double        --makes sure the sum/difference of 2 angles is within [0,2*pi]          --tested
+angleRem v 
+    | v > 2 * pi = v - 2 * pi
+    | v < 0 = v + 2 * pi
+    | otherwise = v
 
---compDiv :: Cplex -> Cplex -> Cplex
---compDiv rect1@('R',a,b) rect2@('R',c,d) = 
---    let pol1 = toPol rect1
---        pol2 = toPol rect2
---    in ('P',(getDist pol1) / (getDist pol2), (getAngle pol1) - (getAngle pol2))
---compDiv rect@('R',a,b) ('P',r,v) = 
---    let pol = toPol rect
---    in ('P',r / (getDist pol),v - (getAngle pol))
---compDiv ('P',r,v) rect@('R',a,b) = 
---    let pol = toPol rect
---    in ('P',r / (getDist rect),v - (getAngle pol))
---compDiv ('P',r1,v1) ('P',r2,v2) = ('P',r1 / r2,v1 - v2)
---compDiv _ _ = error "Invalid argument/s"
+compMult :: Cplex -> Cplex -> Cplex
+compMult rect1@('R',a,b) rect2@('R',c,d) =                  --tested
+    let pol1 = toPol rect1
+        pol2 = toPol rect2
+        distance = getDist pol1 * getDist pol2
+        angle = angleRem (getAngle pol1 + getAngle pol2)
+    in ('P',distance,angle)
+compMult rect@('R',a,b) ('P',r,v) =                         --tested
+    let pol = toPol rect
+        distance = r * getDist pol
+        angle = angleRem (v + getAngle pol)
+    in ('P',distance,angle)
+compMult ('P',r,v) rect@('R',a,b) =                         --tested
+    let pol = toPol rect
+        distance = r * getDist rect
+        angle = angleRem (v + getAngle pol) 
+    in ('P',distance,angle)
+compMult ('P',r1,v1) ('P',r2,v2) =                          --tested
+    let distance = r1 * r2
+        angle = angleRem (v1 + v2) 
+    in ('P',distance,angle)
+compMult _ _ = error "Invalid argument/s"                   --tested
+
+compDiv :: Cplex -> Cplex -> Cplex
+compDiv rect1@('R',a,b) rect2@('R',c,d) =                   --tested
+    let pol1 = toPol rect1
+        pol2 = toPol rect2
+        distance = getDist pol1 / getDist pol2
+        angle = angleRem (getAngle pol1 - getAngle pol2)
+    in ('P',distance, angle)
+compDiv rect@('R',a,b) ('P',r,v) =                          --tested
+    let pol = toPol rect
+        distance = getDist pol / r
+        angle = angleRem (getAngle pol - v)
+    in ('P',distance,angle)
+compDiv ('P',r,v) rect@('R',a,b) =                          --tested
+    let pol = toPol rect
+        distance = r / getDist rect
+        angle = angleRem (v - getAngle pol)
+    in ('P',distance,angle)
+compDiv ('P',r1,v1) ('P',r2,v2) =                           --tested
+    let distance = r1 / r2
+        angle = angleRem (v1 - v2)
+    in ('P',distance,angle)
+compDiv _ _ = error "Invalid argument/s"                    --tested
