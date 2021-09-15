759d14239342460de5443c92971ff380fb8a5bfb
MahmutOsmanovic <47375043+MahmutOsmanovic@users.noreply.github.com>
2021-09-15 12:09:58 +0200

    63 lines, updated filterquad, edited makePol, edited getAngle, added comments, added testfile

--- labOne.hs
+++ labOne.hs
@@ -1,14 +1,13 @@
-type Complex = (Char, Float, Float) 
-type IntegerPair = (Integer,Integer)
+type Complex = (Char, Float, Float)
 
 makeRec :: Float -> Float -> Complex
 makeRec a b = ('R',a,b) -- z = a + b*i
 
 makePol :: Float -> Float -> Complex
-makePol r v
-    | v < 0 || v>2*pi = error "Please, insert an angle v in the range of [0,2*pi]"
-    | r < 0 = error "Please, insert a distance r ub the range of r>= 0"
-    | otherwise = ('P',r,v) -- z = re^(iv)
+makePol r v -- z = r*e^(i(v))
+    | v > 2 * pi && v <= 4 * pi = ('P',abs r,v - 2 * pi)
+    | v < 0 = ('P',abs r,v + 2 * pi)
+    | otherwise = ('P',abs r,v)
 
 getRe :: Complex -> Float
 getRe ('R',a,_) = a
@@ -26,18 +25,12 @@ getDist ('P',r,_) = r
 getDist _ = error "Invalid format. Correct format:\n \t('R' or 'P', float, float), R = Rectangular, P = Polar, with positive angle"
 
 getAngle :: Complex -> Float
-getAngle ('R',a,b) --check this
+getAngle ('R',a,b) 
     | a == 0 && b == 0 || a > 0 && b == 0 = 0 -- edge case
     | a < 0 && b == 0 = pi -- edge case
     | b > 0 = atan2 b a -- (0,pi)
     | b < 0 = 2*pi - abs(atan2 b a) -- (pi,2pi) == (pi,2pi)
-getAngle ('P',r,v) = if v > 0 then v else error "Not Complex"
-
-angleRem :: Float -> Float        --makes sure the sum/difference of 2 angles is within [0,2*pi]
-angleRem v 
-    | v > 2 * pi = v - 2 * pi
-    | v < 0 = v + 2 * pi
-    | otherwise = v
+getAngle ('P',r,v) = if r>0 && v>=0 then v else error "Invalid format. Correct format:\n \t('R' or 'P', float, float), R = Rectangular, P = Polar, with positive angle"
 
 toRec :: Complex -> Complex
 toRec z = makeRec (getRe z) (getIm z)
@@ -52,29 +45,19 @@ compSub :: Complex -> Complex -> Complex
 compSub z1 z2 = makeRec (getRe z1 - getRe z2) (getIm z1 - getIm z2) 
 
 compMult :: Complex -> Complex -> Complex
-compMult z1 z2 = makePol (getDist z1 * getDist z2) (angleRem(getAngle z1 + getAngle z2))
+compMult z1 z2 = makePol (getDist z1 * getDist z2) (getAngle z1 + getAngle z2)
 
 compDiv :: Complex -> Complex -> Complex
-compDiv z1 z2 = makePol (getDist z1 / getDist z2) (angleRem(getAngle z1 - getAngle z2))
+compDiv z1 z2 = makePol (getDist z1 / getDist z2) (getAngle z1 - getAngle z2)
 
-genCompList :: IntegerPair -> IntegerPair -> [Complex]
-genCompList p1 p2 = [('R',fromIntegral re, fromIntegral im)| re <- [fst p1 .. snd p1], im <- [fst p2 .. snd p2]]
+genCompList :: (Integer,Integer) -> (Integer,Integer) -> [Complex] -- get pol form by comb re and im input
+genCompList (a,b) (c,d) = if a > b || c > d then error "Invalid range/s" else [('R',fromIntegral x,fromIntegral y)| x <- [a..b], y <- [c..d]]
 
-listToPol :: [Complex] -> [Complex]
-listToPol inList = [pol | threeTuple <- inList, let pol = makePol (getDist threeTuple) (getAngle threeTuple)]
+listToPol :: [Complex] -> [Complex] -- make all complex inputs on polar form
+listToPol list = [toPol t | t <- list]
 
-filterLengths :: Float -> [Complex] -> [Complex]
+filterLengths :: Float -> [Complex] -> [Complex] -- get all coor in circle within radius k
 filterLengths k xs = [cplex | cplex <- xs, getDist cplex <= k]
 
-filterQuadrant :: Int -> [Complex] -> [Complex]
-filterQuadrant m inList = [cplex | m>=1 && m<=4, cplex <- inList, isInQuadM cplex m]
-
-isInQuadM :: Complex -> Int -> Bool
-isInQuadM z m
-    | re > 0 && im > 0 && m == 1 = True
-    | re < 0 && im > 0 && m == 2 = True
-    | re < 0 && im < 0 && m == 3 = True
-    | re > 0 && im < 0 && m == 4 = True
-    | otherwise = False -- not inside any quad
-        where re = getRe z
-              im = getIm z
+filterQuadrant :: Int -> [Complex] -> [Complex] -- get all coor in quad m
+filterQuadrant m xs = [cplex | m>=1 && m<=4, cplex <- xs, let v = getAngle cplex in v > (fromIntegral m - 1)*pi/2 && v < fromIntegral m * pi / 2]
